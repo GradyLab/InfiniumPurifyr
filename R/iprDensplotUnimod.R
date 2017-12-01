69f92c6df2d3c93fb3e2cbc659331dab2be68d90
smaden <metamaden@users.noreply.github.com>
2017-12-01 10:32:17 -0800

    Update iprDensplotUnimod.R

--- R/iprDensplotUnimod.R
+++ R/iprDensplotUnimod.R
@@ -1,4 +1,4 @@
-ipr.densplotUnimod <- function(df,hyper,hypo,main="Unimodal Methylation by Sample",ymax=10){
+iprDensplotUnimod <- function(df,hyper,hypo,main="Unimodal Methylation by Sample",ymax=10){
   plot(2,1,type="b",xlim=c(0,1),ylim=c(0,ymax),
        xlab="Beta-val",ylab="Density",main=main)
   plotcol <- colours()[sample(nrow(df),nrow(df))]
